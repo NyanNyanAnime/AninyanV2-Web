f8ea88012fdc7d7ee64d478387d7c200ecb07001
Al Hilaluddin <133865308+exoream@users.noreply.github.com>
2025-09-26 16:10:36 +0700

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
-import Home from '../pages/home'
+import Home from '../pages/Home'
 import DetailAnime from '../pages/DetailAnime'
 import VideoAnime from '../pages/VideoAnime'
 import ViewMore from '../pages/ViewMore'
